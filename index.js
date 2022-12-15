5877f2e6ed7b9356b3fe91f46e0e864e0dc71b92
dairedo7 <vladik719@gmail.com>
2022-12-15 11:54:10 +0100

    vercel

---
+++ index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = require('./app');
+
+const { MONGO_URL, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`The server is running. Use our API on port: ${PORT}`);
+    });
+    console.log('Database connection successful');
+  })
+  .catch((error) => {
+    console.log(`The server is not running. Error message: ${error.message}`);
+    process.exit(1);
+  });
